77b99a7a27150d7ad0f784be11af9e787836cc63
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2024-12-15 14:00:36 +0530

    find oldest age

---
+++ python_practice_/find_oldest_age.py
@@ -0,0 +1,8 @@
+# User will input (3ages).Find the oldest one
+
+age1 = int(input("Enter your age: "))
+age2 = int(input("Enter your age: "))
+age3 = int(input("Enter your age: "))
+
+max_age = max(age1, age2, age2)
+print(f"Oldest one is {max_age}")
