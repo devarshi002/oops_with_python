04c0e690f86fee9186b227bf5b03b3974926c670
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2024-12-26 11:33:57 +0530

    sum of elem

---
+++ leetcode/SumOfElem.py
@@ -0,0 +1,6 @@
+def sum_of_array(arr):
+    return sum(arr)
+
+array=[2,4,6,8]
+result=sum_of_array(array)
+print('Sum:', result)
---
+++ leetcode/sum_of_ele.py
@@ -0,0 +1,9 @@
+def sum_of_array(arr):
+    total=0
+    for num in arr:
+        total +=num
+    return total
+
+array=[2,4,5,6]
+result=sum_of_array(array)
+print(result)
