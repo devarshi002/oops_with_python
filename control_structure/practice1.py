60b191f8db0bc28a97ef35e822c1ceba89f91878
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2025-01-05 13:12:39 +0530

    Control Structure

---
+++ control_structure/practice1.py
@@ -0,0 +1,10 @@
+# conditional statement
+
+number = int(input("Enter a number:"))
+
+if number > 0:
+    print("Positive number")
+elif number < 0:
+    print("Negative number")
+else:
+    print("Number is zero")
